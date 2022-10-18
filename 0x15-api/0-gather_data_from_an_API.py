988eb5ce56b5dad47437d059ce512f33f5e5a1c9
akakiplimo <akakiplimo@gmail.com>
2022-10-18 10:35:07 -0700

    gather API data fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-"""
-For a given employee ID,
-returns information about an employees TODO list progress
-"""
+"""Returns to-do list information for a given employee ID"""
 import requests
 import sys
-
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
@@ -15,4 +11,4 @@ if __name__ == "__main__":
     completed = [t.get("title") for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(title)) for title in completed]
+    [print("\t {}".format(c)) for c in completed]
