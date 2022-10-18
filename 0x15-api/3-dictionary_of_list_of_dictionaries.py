2e6363987d24dcd1c367c9632dbe99ecb2376fd1
akakiplimo <akakiplimo@gmail.com>
2022-10-18 10:36:29 -0700

    Dicts of list of dicts fix

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""Exports all employees todo list data in the JSON format"""
+""" Exports all employees todo list data in the JSON format """
 import json
 import requests
-
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     users = requests.get(url + "users").json()
 
-    with open("todo_all_employees.json", "w") as jsonFile:
+    with open("todo_all_employees.json", "w") as jsonfile:
         json.dump({
             u.get("id"): [{
-                "task": task.get("title"),
-                "completed": task.get("completed"),
+                "task": t.get("title"),
+                "completed": t.get("completed"),
                 "username": u.get("username")
-            } for task in requests.get(url + "todos",
-                                       params={"userId": u.get("id")}).json()]
-            for u in users}, jsonFile)
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
